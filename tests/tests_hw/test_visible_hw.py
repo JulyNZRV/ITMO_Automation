2b57487b78c81fada2f1fc22fc12286947e75350
Yuliya <julinaz@mail.ru>
2025-08-04 21:07:16 +0300

    HW_8

--- tests/tests_hw/test_visible_hw.py
+++ tests/tests_hw/test_visible_hw.py
@@ -1,33 +1,24 @@
-import time
 from pages.accordion import AccordionPage
-
+import time
 
 def test_visible_accordion(browser):
-    # Инициализация и переход на страницу
     page = AccordionPage(browser)
     page.load()
 
-    # Проверка видимости контента секции 1
     content = page.get_element(AccordionPage.section1_content)
     assert content.is_displayed(), "Контент секции 1 должен быть видим"
 
-    # Клик по заголовку секции 1
     heading = page.get_element(AccordionPage.section1_heading)
     heading.click()
 
-    # Пауза для анимации
     time.sleep(2)
 
-    # Проверка скрытия контента секции 1
     assert not content.is_displayed(), "Контент секции 1 должен быть скрыт"
 
-
 def test_visible_accordion_default(browser):
-    # Инициализация и переход на страницу
     page = AccordionPage(browser)
     page.load()
 
-    # Проверка скрытых элементов по умолчанию
     elements = [
         page.get_element(AccordionPage.section2_content_p1),
         page.get_element(AccordionPage.section2_content_p2),
