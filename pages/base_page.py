fc63b39bd2750973de36c7f17a6575024d84bb7c
Yuliya <julinaz@mail.ru>
2025-08-20 00:22:40 +0300

    task_12

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,14 @@
-from selenium.common import NoAlertPresentException
 from selenium.webdriver.common.by import By
 import logging
+from components.components import WebElement
+
 
 class BasePage:
 
     def __init__(self, driver, base_url):
         self.driver = driver
         self.base_url = base_url
+        self.metaView = WebElement(driver, 'head > meta[name="viewport"]')
 
     def visit(self):
         return self.driver.get(self.base_url)
